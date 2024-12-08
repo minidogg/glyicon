edbcbede49b628da94f2dc6127981dcd5833ad8f
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-12-08 14:33:21 -0600

    rename main.js to lib.js

--- lib.js
+++ lib.js
@@ -1,5 +1,4 @@
-const canvas = document.getElementById("output")
-const ctx = canvas.getContext("2d")
+
 
 function ResizeCanvas(){
     let minMeasure = Math.min(window.innerHeight, window.innerWidth)
@@ -34,8 +33,3 @@ async function GenerateGlyiconData(text){
 
     return {r, g, b, grid}
 }
-let data = await GenerateGlyiconData("unluckycrafter")
-console.log(data)
-
-ctx.fillStyle = `rgb(${data.r}, ${data.g}, ${data.b})`
-ctx.fillRect(0, 0, 100, 100)

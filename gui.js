0f505c8d9290dccb3ccd39f57ab2e0ce0ddf6b5c
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-12-08 14:50:15 -0600

    all done

--- gui.js
+++ gui.js
@@ -9,13 +9,22 @@ function ResizeCanvas(){
 }
 ResizeCanvas()
 
-let data = await GenerateGlyiconData("unluckycrafter", 5, 5)
-console.log(data)
+async function UpdateGlyicon(text){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    let data = await GenerateGlyiconData(text, 5, 5)
+    console.log(data)
 
-ctx.fillStyle = `rgb(${data.r}, ${data.g}, ${data.b})`
-let pixelSize = canvas.width/5
-for(let i1 = 0;i1<data.grid.length;i1++){
-    for(let i2 = 0;i2<data.grid[i1].length;i2++){
-        if(data.grid[i1][i2]==true)ctx.fillRect(i2*pixelSize, i1*pixelSize, pixelSize, pixelSize)
+    ctx.fillStyle = `rgb(${data.r}, ${data.g}, ${data.b})`
+    let pixelSize = canvas.width/5
+    for(let i1 = 0;i1<data.grid.length;i1++){
+        for(let i2 = 0;i2<data.grid[i1].length;i2++){
+            if(data.grid[i1][i2]==true)ctx.fillRect(i2*pixelSize, i1*pixelSize, pixelSize, pixelSize)
+        }
     }
+
 }
+
+let input = document.getElementById("inputText")
+input.addEventListener("change", ()=>{
+    UpdateGlyicon(input.value)
+})
